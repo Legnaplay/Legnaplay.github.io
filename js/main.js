2dd029fe20d66126bf8bbced2002005a12426865
Legnaplay <88263224+Legnaplay@users.noreply.github.com>
2021-09-29 22:27:14 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ const typed = new Typed('.typed', {
     strings: ['<i class="name"> Angel</i>'],
     typeSpeed: 95,
     startDelay: 300,
-    loop: false,
+    loop: true,
     showCursor: true,
 });
